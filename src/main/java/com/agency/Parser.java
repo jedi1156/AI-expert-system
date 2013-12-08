3e0bedb4217d34dd49874b6fcb431c71340c9f8e
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-08 17:44:12 +0100

    Removed old Place class

--- src/main/java/com/agency/Parser.java
+++ src/main/java/com/agency/Parser.java
@@ -13,28 +13,6 @@ import java.util.Map;
 
 public class Parser {
 	
-	public static ArrayList<Place> parsePlaces(String filename) {
-		ArrayList<Place> result = new ArrayList<Place>();
-		Yaml yaml = new Yaml();
-    	InputStream input;
-    	Map<Object, Object> data;
-		try {
-			input = new FileInputStream(new File(filename));
-	        data = (Map<Object, Object>) yaml.load(input);
-	        
-	        for (Map.Entry<Object, Object> entry : data.entrySet()) {
-	        	Map<String, Object> place_map = (Map<String, Object>) entry.getValue();
-	            result.add(new Place((String)(place_map).get("name")));
-	            
-	            System.out.println((Boolean)place_map.get("sea"));
-	        }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		return result;
-	}
-	
 	public static Map<String, Question> parseQuestions(String filename) {
 		Yaml yaml = new Yaml();
     	InputStream input;
