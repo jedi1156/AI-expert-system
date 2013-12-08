7c43e1222b63992a112564ba30290d16ea45f5d6
Tomasz Pewiński <pewniak747@gmail.com>
2013-12-08 18:05:49 +0100

    numeric question

--- src/main/java/com/agency/Communication.java
+++ src/main/java/com/agency/Communication.java
@@ -14,7 +14,28 @@ public class Communication {
 
 	public static void askQuestion(String questionName) {
 		Question question = questionData.get(questionName);
+		if (question.type.equals("boolean")) {
+			askBooleanQuestion(question);
+		}
+		else if (question.type.equals("numeric")) {
+			askNumericQuestion(question);
+		}
+	}
+	
+	static void askBooleanQuestion(Question question) {
 		int result = JOptionPane.showConfirmDialog(null, question.message, "Choose an answer", JOptionPane.YES_NO_OPTION);
+		if (result == JOptionPane.YES_OPTION) {
+			// TODO: handle YES
+		}
+		else {
+			// TODO handle NO
+		}
+	}
+	
+	static void askNumericQuestion(Question question) {
+		String data = JOptionPane.showInputDialog(null, question.message);
+		int result = Integer.parseInt(data);
+		// TODO handle result
 	}
 	
 	static Map<String, String> messageData = Parser.parseMessages("assets/messages.yml");
