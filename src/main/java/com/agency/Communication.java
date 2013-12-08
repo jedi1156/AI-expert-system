b573bb7c2591908c14e8f9066213f019ba411a66
Tomasz Pewiński <pewniak747@gmail.com>
2013-12-08 18:14:06 +0100

    stoopid single choice selection

--- src/main/java/com/agency/Communication.java
+++ src/main/java/com/agency/Communication.java
@@ -1,10 +1,13 @@
 package com.agency;
 
 import java.util.Map;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
 
 public class Communication {
 	public static void showMessage(String messageName) {
@@ -19,6 +22,9 @@ public class Communication {
 		}
 		else if (question.type.equals("numeric")) {
 			askNumericQuestion(question);
+		}
+		else if (question.type.equals("multiple")) {
+			askMultipleQuestion(question);
 		}
 	}
 	
@@ -36,6 +42,19 @@ public class Communication {
 		String data = JOptionPane.showInputDialog(null, question.message);
 		int result = Integer.parseInt(data);
 		// TODO handle result
+	}
+	
+	static void askMultipleQuestion(Question question) {
+		ArrayList<String> answers = new ArrayList<String>();
+		answers.add("opt 1");
+		answers.add("opt 2");
+		answers.add("opt 3");
+		answers.add("opt 4");
+		JList list = new JList(answers.toArray());
+	    list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); 
+		Object[] options = {question.message, list};
+		JOptionPane.showMessageDialog(null, options, "Choose an answer", JOptionPane.PLAIN_MESSAGE);
+		int result = list.getSelectedIndices()[0];
 	}
 	
 	static Map<String, String> messageData = Parser.parseMessages("assets/messages.yml");
