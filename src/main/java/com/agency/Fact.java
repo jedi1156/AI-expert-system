1bfe47004abd22ce41c5e3a2587401f07ec5b6dd
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-09 01:27:49 +0100

    Less, Greater operators

--- src/main/java/com/agency/Fact.java
+++ src/main/java/com/agency/Fact.java
@@ -5,39 +5,47 @@ public class Fact {
 	private boolean booleanValue;
 	private String stringValue;
 	private int intValue;
-	
+
 	Fact(String name) {
 		this.name = name;
 	}
-	
+
 	Fact(String name, boolean booleanValue) {
 		this(name);
 		this.booleanValue = booleanValue;
 	}
-	
+
 	Fact(String name, String stringValue) {
 		this(name);
 		this.stringValue = stringValue;
 	}
-	
+
 	Fact(String name, int intValue) {
 		this(name);
 		this.intValue = intValue;
 	}
-	
+
 	public boolean name(String value) {
 		return name.equals(value);
 	}
-	
+
 	public boolean value(boolean value) {
 		return booleanValue == value;
 	}
-	
+
 	public boolean value(String value) {
 		return value.equals(stringValue);
 	}
-	
+
 	public boolean value(int value) {
 		return value == intValue;
+	}
+
+	public boolean intValueLessThan(int value) {
+		return intValue < value;
+	}
+
+	public boolean intValueGreaterThan(int value) {
+		return intValue > value;
 	}
 }
